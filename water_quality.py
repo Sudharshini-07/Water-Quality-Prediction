886e367f224b917e11e590cc33836c0bd1f74be4
Sudharshini R <rsudharshini6@gmail.com>
2025-07-10 15:16:17 +0530

    water_quality.py

--- water_quality.py
+++ water_quality.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1DQN8fjwk567rH9-Vbmzohjp1hsqaT5ga
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
